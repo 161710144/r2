52890240daaee63b8cbf60eee6741ea7bc29f8af
unknown <161710116@smkassalaambandung.sch.id>
2017-10-24 17:33:05 +0700

    latihan 2

---
+++ tt1.php
@@ -0,0 +1,15 @@
+<?php
+class hewan{
+public $herbivora;
+public $karnivora;
+public $omnivora;
+
+
+public function __construct($herbivora,$karnivora,$omnivora){
+$this->herbivora = $herbivora;
+$this->karnivora = $karnivora;
+$this->omnivora = $omnivora;
+}
+}
+
+?>
---
+++ tt2.php
@@ -0,0 +1,11 @@
+<center>
+<?php
+require_once'tt1.php';
+
+$h1 = new hewan("Sapi","Harimau","Kucing");
+echo "Hewan herbivora adalah ".$h1->herbivora.'<br>';
+echo "Hewan karnivora adalah ".$h1->karnivora.'<br>';
+echo "Hewan omnivora adalah ".$h1->omnivora.'<br>';
+//echo "Melahirkan di ".$bio1->tempat_lahir.'<br>';
+?>
+</center>
